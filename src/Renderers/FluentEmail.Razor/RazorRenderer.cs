75a585220490fcf613bf79a8cdc5c340313b5bc7
Leniel Maccaferri <leniel@gmail.com>
2018-10-22 23:26:21 -0300

    Fixed Razor Renderer

--- src/Renderers/FluentEmail.Razor/RazorRenderer.cs
+++ src/Renderers/FluentEmail.Razor/RazorRenderer.cs
@@ -10,29 +10,16 @@ namespace FluentEmail.Razor
     public class RazorRenderer : ITemplateRenderer
     {
         public async Task<string> ParseAsync<T>(string template, T model, bool isHtml = true)
-        {            
-            var engine = new RazorLightEngineBuilder()
-                .UseMemoryCachingProvider()
-                .Build();            
+        {
+            var project = new InMemoryRazorLightProject();
+            var engine = new RazorLightEngineBuilder().UseProject(project).Build();
 
-            return await engine.CompileRenderAsync<T>(GetHashString(template), template, model);
+            return await engine.CompileRenderStringAsync<T>(Guid.NewGuid().ToString(), template, model);
         }
 
         string ITemplateRenderer.Parse<T>(string template, T model, bool isHtml)
         {
             return ParseAsync(template, model, isHtml).GetAwaiter().GetResult();
-        }
-
-        public static string GetHashString(string inputString)
-        {
-            StringBuilder sb = new StringBuilder();
-            var hashbytes = SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(inputString));
-            foreach (byte b in hashbytes)
-            {
-                sb.Append(b.ToString("X2"));
-            }
-
-            return sb.ToString();
         }
     }
 }
